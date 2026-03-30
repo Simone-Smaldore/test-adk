14cc1531b5badbde0a5aed380fee27c54d400937
Simone-Smaldore <57629587+Simone-Smaldore@users.noreply.github.com>
2026-03-30 08:28:07 +0000

    Aggiunti file main

---
+++ main_v1.py
@@ -0,0 +1,110 @@
+import os
+import asyncio
+from google.adk.agents import Agent
+from google.adk.models.lite_llm import LiteLlm  # For multi-model support
+from google.adk.sessions import InMemorySessionService
+from google.adk.runners import Runner
+from google.genai import types  # For creating message Content/Parts
+from dotenv import load_dotenv
+
+import warnings
+
+from src.weather_agent_v1.agent import weather_agent_v1
+# Ignore all warnings
+warnings.filterwarnings("ignore")
+
+import logging
+
+logging.basicConfig(level=logging.ERROR)
+
+APP_NAME = "weather_tutorial_app"
+USER_ID = "user_1"
+SESSION_ID = "session_001"
+
+session_service = InMemorySessionService()
+load_dotenv()
+
+
+async def init_session(app_name: str, user_id: str,
+                       session_id: str) -> InMemorySessionService:
+
+    session = await session_service.create_session(app_name=app_name,
+                                                   user_id=user_id,
+                                                   session_id=session_id)
+    print(
+        f"Session created: App='{app_name}', User='{user_id}', Session='{session_id}'"
+    )
+    return session
+
+
+async def call_agent_async(query: str, runner, user_id, session_id):
+    """Sends a query to the agent and prints the final response."""
+    print(f"\n>>> User Query: {query}")
+
+    # Prepare the user's message in ADK format
+    content = types.Content(role='user', parts=[types.Part(text=query)])
+
+    final_response_text = "Agent did not produce a final response."  # Default
+
+    # Key Concept: run_async executes the agent logic and yields Events.
+    # We iterate through events to find the final answer.
+    async for event in runner.run_async(user_id=user_id,
+                                        session_id=session_id,
+                                        new_message=content):
+        # You can uncomment the line below to see *all* events during execution
+        # print(
+        #     f"  [Event] Author: {event.author}, Type: {type(event).__name__}, Final: {event.is_final_response()}, Content: {event.content}"
+        # )
+
+        # Key Concept: is_final_response() marks the concluding message for the turn.
+        if event.is_final_response():
+            if event.content and event.content.parts:
+                # Assuming text response in the first part
+                final_response_text = event.content.parts[0].text
+            elif event.actions and event.actions.escalate:  # Handle potential errors/escalations
+                final_response_text = f"Agent escalated: {event.error_message or 'No specific message.'}"
+            # Add more checks here if needed (e.g., specific error codes)
+            break  # Stop processing events once the final response is found
+
+    print(f"<<< Agent Response: {final_response_text}")
+
+
+async def run_conversation(runner: Runner):
+    await call_agent_async("What is the weather like in London?",
+                           runner=runner,
+                           user_id=USER_ID,
+                           session_id=SESSION_ID)
+
+    await call_agent_async("How about Paris?",
+                           runner=runner,
+                           user_id=USER_ID,
+                           session_id=SESSION_ID
+                           )  # Expecting the tool's error message
+
+    await call_agent_async("Tell me the weather in New York",
+                           runner=runner,
+                           user_id=USER_ID,
+                           session_id=SESSION_ID)
+
+
+def main():
+    print("Hello from test-adk!")
+
+    session = asyncio.run(init_session(APP_NAME, USER_ID, SESSION_ID))
+    print(
+        f"Session created: App='{APP_NAME}', User='{USER_ID}', Session='{SESSION_ID}'"
+    )
+
+    # --- Runner ---
+    # Key Concept: Runner orchestrates the agent execution loop.
+    runner = Runner(
+        agent=weather_agent_v1,  # The agent we want to run
+        app_name=APP_NAME,  # Associates runs with our app
+        session_service=session_service  # Uses our session manager
+    )
+    print(f"Runner created for agent '{runner.agent.name}'.")
+    asyncio.run(run_conversation(runner=runner))
+
+
+if __name__ == "__main__":
+    main()
